10dbb074ab3c3ba8349785e3d2b7213805532fa8
plake <plake492@gmail.com>
2019-08-26 17:38:48 -0400

    added people images

--- server.js
+++ server.js
@@ -22,9 +22,10 @@ const people = [
   {
     routeName: "patricklake",
     name: "Patrick Lake",
-    q1:5
-    // q2:3,
-    // q3:5,
+    gender: "male",
+    image: "https://scontent-ort2-1.xx.fbcdn.net/v/t31.0-8/22792269_10215150326294956_4078608217278741823_o.jpg?_nc_cat=110&_nc_oc=AQmKAINK-iOKKc9bxU10ru-4gGfflBOS2BjQBgR7vlc3LrqDB4nWUxHEYL9Xz_91CUo&_nc_ht=scontent-ort2-1.xx&oh=ffe4614586c81a4e78aeb513589eed97&oe=5DD6320D",
+    q1:3,
+    q2:5,
     // q4:1,
     // q5:2,
     // attack: 100,
@@ -33,9 +34,10 @@ const people = [
   {
     routeName: "victorialake",
     name: "Victoria Lake",
-    q1:5
-    // q2:3,
-    // q3:5,
+    gender: "female",
+    image: "https://scontent-ort2-1.xx.fbcdn.net/v/t1.0-9/60357727_2758951420843167_3516734323069812736_n.jpg?_nc_cat=107&_nc_oc=AQm8FjXruFVjMbhvzbFK2MzZHNOR4adO6QPbe4wBAISZdFGlSqdX-qzidDlWgmcEHuQ&_nc_ht=scontent-ort2-1.xx&oh=d682c65f9e4a142385d6a66af0abedfb&oe=5E0BA430",
+    q1:3,
+    q2:5,
     // q4:1,
     // q5:2,
     // attack: 100,
@@ -44,10 +46,10 @@ const people = [
   {
     routeName: "dangorden",
     name: "Dan Gorden",
-    q1: 5
-    // q2:3,
-    // q3:5,
-    // q4:1,
+    gender: "male", 
+    image: "https://scontent-ort2-1.xx.fbcdn.net/v/t1.0-9/64577341_10162973682210643_7307563876898308096_o.jpg?_nc_cat=110&_nc_oc=AQnt1zUE4vWbpTlEJQQpZGUB67ouMFGJ7cDPDyCC3xcfn3R4IPw0VP9TI3CrR5wiiaM&_nc_ht=scontent-ort2-1.xx&oh=4e43d056bb964108d7bb40149754328d&oe=5DD3FD2B",
+    q1:5,
+    q2:1,
     // q5:2, 
     // attack: 200,
     // defense: 20
@@ -59,9 +61,14 @@ app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
+app.get("/all", function(req, res) {
+  res.sendFile(path.join(__dirname, "all.html"));
+})
+
 app.get("/api/people", function(req, res) {
   return res.json(people);
 });
+
 
 app.get("/api/people/:people", function(req, res) {
   const chosen = req.params.people
